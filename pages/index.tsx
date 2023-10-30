4d31d68cad0f4dfaa43c62c32f749ea9c88751ab
Pat Wangrungarun <phatograph@gmail.com>
2023-10-30 17:39:39 +0900

    endBattle

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,54 @@ import * as React from 'react'
 // import {useRouter} from 'next/router'
 
 const Index = ({}) => {
+  const [__count, __countSet] = React.useState(10)
+
+  const endBattle = () => {
+    if (
+      window.webkit &&
+      window.webkit.messageHandlers.bridge &&
+      window.webkit.messageHandlers.bridge.postMessage
+    ) {
+      window.webkit.messageHandlers.bridge.postMessage('{"event": "finish"}')
+    }
+
+    if (window.Android && window.Android.onButtonClick) {
+      window.Android.onButtonClick('skipBtn')
+    }
+  }
+
+  const ticker = () => {
+    __countSet((prevState) => {
+      return prevState - 1
+    })
+  }
+
+  React.useEffect(() => {
+    if (__count > 0) {
+      window.setTimeout(() => {
+        ticker()
+      }, 1000)
+    }
+  }, [__count])
+
   return (
     <div className='Index'>
-      <a
-        className='Button'
-        onClick={() => {
-          if (
-            window.webkit &&
-            window.webkit.messageHandlers.bridge &&
-            window.webkit.messageHandlers.bridge.postMessage
-          ) {
-            window.webkit.messageHandlers.bridge.postMessage(
-              '{"event": "finish"}'
-            )
-          }
+      <div className='Index__wrapper'>
+        <a
+          className='Button'
+          onClick={() => {
+            endBattle()
+          }}
+        >
+          Skip battle
+        </a>
 
-          if (window.Android && window.Android.onButtonClick) {
-            window.Android.onButtonClick('skipBtn')
-          }
-        }}
-      >
-        Skip battle
-      </a>
+        <p className='Index__p'>
+          {__count > 0
+            ? `Battle will automatically end in ${__count} seconds.`
+            : 'Battle ended.'}
+        </p>
+      </div>
     </div>
   )
 }
